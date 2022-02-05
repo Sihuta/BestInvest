de9abe18edb3fc2c7b2d2c60a35d14c8bac466eb
Сігута Діана <diana.sihuta@nure.ua>
2022-02-05 05:47:23 +0200

    Implement the opportunity to get teams in which startuper participates.

---
+++ BestInvest.API/BLL/Interfaces/ITeamService.cs
@@ -0,0 +1,10 @@
+﻿using BestInvest.API.BLL.DTO;
+using System.Security.Claims;
+
+namespace BestInvest.API.BLL.Interfaces
+{
+    public interface ITeamService
+    {
+        Task<List<TeamDTO>> GetCreatedTeamsAsync(ClaimsPrincipal user);
+    }
+}
