125056b4b1a6bb9e66b1d2dfcc8419dcc75098b4
Сігута Діана <diana.sihuta@nure.ua>
2022-02-05 05:46:20 +0200

    Implement the opportunity to get categories by different params.

---
+++ BestInvest.API/BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BestInvest.API.BLL.DTO;
+
+namespace BestInvest.API.BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDTO>> GetAsync();
+        Task<List<CategoryDTO>> GetByProjectIdAsync(int id);
+        Task<List<CategoryDTO>> GetByInvestorIdAsync(int id);
+    }
+}
---
+++ BestInvest.API/BLL/Services/CategoryService.cs
@@ -0,0 +1,47 @@
+﻿using BestInvest.API.BLL.DTO;
+using BestInvest.API.BLL.Interfaces;
+using BestInvest.API.DAL.EF;
+using BestInvest.API.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestInvest.API.BLL.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext dbContext;
+        private readonly Mapper mapper;
+
+        public CategoryService(AppDbContext appDbContext, Mapper mapper)
+        {
+            dbContext = appDbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<CategoryDTO>> GetAsync()
+        {
+            return mapper.Map<Category, CategoryDTO>(await dbContext.Categories.ToListAsync());
+        }
+
+        public async Task<List<CategoryDTO>> GetByInvestorIdAsync(int id)
+        {
+            var investorCategories = dbContext.InvestorCategories
+                .Include(ic => ic.Category)
+                .Where(ic => ic.AccountId == id);
+
+            return await investorCategories
+                .Select(ic => new CategoryDTO { Id = ic.Category.Id, Name = ic.Category.Name})
+                .ToListAsync();
+        }
+
+        public async Task<List<CategoryDTO>> GetByProjectIdAsync(int id)
+        {
+            var projectCategories = dbContext.ProjectCategories
+                .Include(pc => pc.Category)
+                .Where(pc => pc.ProjectId == id);
+
+            return await projectCategories
+                .Select(pc => new CategoryDTO { Id = pc.Category.Id, Name = pc.Category.Name })
+                .ToListAsync();
+        }
+    }
+}
